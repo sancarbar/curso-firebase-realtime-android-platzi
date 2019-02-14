2cfe2115d7b8ad908e75f2d39e742bc08fe75417
sancarbar <sancarbar@gmail.com>
2019-02-14 11:05:11 -0500

    solved Android X issues.

--- app/src/main/java/com/platzi/realtimetrader/MainActivity.kt
+++ app/src/main/java/com/platzi/realtimetrader/MainActivity.kt
@@ -1,7 +1,7 @@
-package com.platzi.android
+package com.platzi.realtimetrader
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
