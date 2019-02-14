96c2636c68d9f456126a0b23f69274ad37d09987
sancarbar <sancarbar@gmail.com>
2019-02-14 11:52:38 -0500

    Implementación escritura de documentos a Firestore.

--- app/src/main/java/com/platzi/realtimetrader/ui/activity/TraderActivity.kt
+++ app/src/main/java/com/platzi/realtimetrader/ui/activity/TraderActivity.kt
@@ -2,16 +2,21 @@ package com.platzi.realtimetrader.ui.activity
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.firestore.FirebaseFirestore
 import com.platzi.realtimetrader.R
 import com.platzi.realtimetrader.model.Crypto
+import com.platzi.realtimetrader.model.User
 import com.platzi.realtimetrader.network.Callback
 import com.platzi.realtimetrader.network.FirebaseService
 import com.platzi.realtimetrader.ui.adapter.CryptosAdapter
 import com.platzi.realtimetrader.ui.adapter.CryptosAdapterListener
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_trader.*
 
 /**
@@ -24,10 +29,19 @@ class TraderActivity : AppCompatActivity(), CryptosAdapterListener {
 
     private val cryptosAdapter: CryptosAdapter = CryptosAdapter(this)
 
+    private var username: String? = null
+
+    var user: User? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trader)
         firebaseService = FirebaseService(FirebaseFirestore.getInstance())
+
+        username = intent.extras!![USERNAME_KEY]!!.toString()
+        usernameTextView.text = username
+
         configureRecyclerView()
         loadCryptos()
     }
@@ -48,6 +62,36 @@ class TraderActivity : AppCompatActivity(), CryptosAdapterListener {
                     cryptosAdapter.cryptosList = result!!
                     cryptosAdapter.notifyDataSetChanged()
 
+                    firebaseService.findUserById(username!!, object : Callback<User> {
+
+                        override fun onSuccess(result: User?) {
+                            user = result
+                            Log.d("Developer", "User:  ${result!!.username}")
+                            if (user!!.cryptosList == null) {
+
+                                val userCryptoList = mutableListOf<Crypto>()
+
+                                for (crypto in cryptosAdapter.cryptosList) {
+                                    val cryptoUser = Crypto()
+                                    cryptoUser.name = crypto.name
+                                    cryptoUser.available = 0
+                                    cryptoUser.imageUrl = crypto.imageUrl
+                                    userCryptoList.add(cryptoUser)
+                                }
+
+                                user!!.cryptosList = userCryptoList
+                                firebaseService.updateUser(user!!, null)
+                            }
+                            loadUserCryptos()
+                        }
+
+                        override fun onFailed(exception: Exception) {
+                            Log.e("Developer", "error", exception)
+                            showGeneralServerErrorMessage()
+                        }
+
+                    })
+
                 }
             }
 
@@ -67,5 +111,21 @@ class TraderActivity : AppCompatActivity(), CryptosAdapterListener {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
+    fun loadUserCryptos() {
+        if (user != null && user!!.cryptosList != null) {
+            infoPanel.removeAllViews()
+            for (crypto in user!!.cryptosList!!) {
+                addUserCryptoInfoRow(crypto)
+            }
+        }
+    }
+
+    private fun addUserCryptoInfoRow(crypto: Crypto) {
+        val view = LayoutInflater.from(this).inflate(R.layout.coin_info, infoPanel, false)
+        view.findViewById<TextView>(R.id.coinLabel).text =
+            getString(R.string.coin_info, crypto.name, crypto.available.toString())
+        Picasso.get().load(crypto.imageUrl).into(view.findViewById<ImageView>(R.id.coinIcon))
+        infoPanel.addView(view)
+    }
 
 }
